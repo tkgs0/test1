004af6b3343b6a995258dc8287dd0df3f9643706
tkgs0 <hgc.sakuya@ya.ru>
2023-01-12 14:27:16 +0000

    commit

--- fakemsg.py
+++ fakemsg.py
@@ -14,7 +14,9 @@ class Funny():
             name = unescape(args[1])
             repo = unescape(args[2].replace("\\n","\n"))
             if r := re.search('\[CQ:image[^\[\]]*\]',repo):  # type: ignore
-                repo.replace(r.group(), f'{MessageSegment.image(r.group())}')
+                r = r if not 'http' in r.group() else re.search('http[^\[\],]*,?', r.group())  # type: ignore
+                repo = repo.replace(r.group(), f'{MessageSegment.image(file=r.group().strip(","))}')  # type: ignore
             dic = {"type": "node", "data": {"name": name, "uin": qq, "content": repo}}
             node.append(dic)
         return node
+

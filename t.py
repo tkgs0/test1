3a1835647bcd49544000d3d16112d8bd8eb41630
tkgs0 <hgc.sakuya@ya.ru>
2023-01-13 07:35:50 +0000

    commit

---
+++ t.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+
+import time
+from pathlib import Path
+try:
+    import ujson as json
+except ModuleNotFoundError:
+    import json
+
+
+filepath = Path('.').rglob(r'*.json')
+
+
+start = time.time()
+print('loading...')
+x = 0
+n = len(filepath := list(filepath))
+
+if not n:
+    exit(-1)
+
+for _file in filepath:
+    x += 1
+    y = int(float(x) / n * 20)
+    print(
+        f"\r[{'='*y}{' '*(20-y)}] {y*5}% {time.time()-start:.2f}s",
+        end=' ', flush=True
+    )
+
+    try:
+        content = json.loads(_file.read_text('utf-8'))
+        _file.write_text(
+            json.dumps(
+                content, ensure_ascii=False,
+                escape_forward_slashes=False,
+                indent=2
+            ),
+            encoding='utf-8'
+        )
+    except:
+        continue
+
+print('done.')

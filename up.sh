896b36cf49ba39f0049ecacce9fe7371bda145a6
tkgs0 <hgc.sakuya@ya.ru>
2023-01-24 04:41:56 +0000

    commit

--- up.sh
+++ up.sh
@@ -20,7 +20,5 @@ screen -r goo -X stuff "vim ~/Zero/plugin/thesaurus/chat.go\n"
 sleep 3
 screen -r goo -X stuff ":%s/seg))/seg), zero\\.OnlyToMe)/g\n:wq\n"
 sleep 3
-screen -r goo -X quit && go mod tidy && go build -ldflags "-s -w" -trimpath -o zbp && GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o zbp.exe && cp zbp.exe /setu/ && screen -r zbp -X quit
-sleep 3
-cp zbp ~/bot/zbp/
+screen -r goo -X quit && go mod tidy && go build -ldflags "-s -w" -trimpath -o zbp && GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -trimpath -o zbp.exe && cp zbp.exe /setu/ && screen -r zbp -X quit && cp zbp ~/bot/zbp/
 
